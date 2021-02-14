00c89d50934de65e5445f08ee91e727ce916a31e
nima789 <77829771+nima789@users.noreply.github.com>
2021-02-14 07:08:26 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+(async () => {
+  console.log("textnow bot start...");
+  const path = require("path");
+  const fs = require("fs").promises;
+  const puppeteer = require("puppeteer");
+  const textNowHelper = require("./utils/helper");
+  const config = require("./config");
+  const {
+    username,
+    password,
+    recipient,
+    message,
+    cookies_str,
+  } = config;
+
+  let browser = null;
+  let page = null;
+
+  try {
+    browser = await puppeteer.launch({ 
+      headless: true
+    });
+    page = await browser.newPage();
+    const client = await page.target().createCDPSession();
+    let cookies = null;
+
+    // Importing exsiting cookies from file
+    try {
+      console.log("Importing existing cookies...");
+      const cookiesJSON = await fs.readFile(
+        path.resolve(__dirname, ".cahce/cookies.json")
+      );
+      cookies = JSON.parse(cookiesJSON);
+    } catch (error) {
+      console.log("Failed to import existing cookies.");
+      if (cookies_str) cookies = parseCookies(cookies_str, 'www.textnow.com');
+    }
+
+    // Log into TextNow and get cookies
+    try {
+      console.log("Logging in with existing cookies");
+      await page.setCookie(...cookies);
+      cookies = await textNowHelper.logIn(page, client);
+    } catch (error) {
+      console.log("Failed to log in with existing cookies.");
+      console.log("Logging in with account credentials...");
+      cookies = await textNowHelper.logIn(page, client, username, password);
+    }
+
+    try {
+      console.log("Successfully logged into TextNow!");
+      // Save cookies to file
+      await fs.writeFile(
+        path.resolve(__dirname, ".cache/cookies.json"),
+        JSON.stringify(cookies)
+      );
+    } catch (error) {
+      console.log("Failed to save cookies to file.");
+    }
+
+    // Select a conversation using recipient info
+    console.log("Selecting conversation...");
+    await textNowHelper.selectConversation(page, recipient);
+
+    // Send a message to the current recipient
+    console.log("Sending message...");
+    await textNowHelper.sendMessage(page, message);
+
+    console.log("Message sent!");
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+
+    if (page) {
+      await page.screenshot({ path: "./error-screenshot.jpg", type: "jpeg" });
+    }
+
+    if (browser) {
+      await browser.close();
+    }
+
+    process.exit(1);
+  }
+})();
+
+function parseCookies(cookies_str, domain) {
+    return cookies_str.split(';').map(pair => {
+        let name = pair.trim().slice(0, pair.trim().indexOf('='));
+        let value = pair.trim().slice(pair.trim().indexOf('=') + 1);
+        return { name, value, domain };
+    });
+};
+
